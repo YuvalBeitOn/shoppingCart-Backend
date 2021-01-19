0088dce8fdaf1926f3e7aae5308016affc23f582
Yuval Beit On <yuvalbeiton@gmail.com>
2021-01-19 11:25:34 +0200

    uploading

---
+++ public/precache-manifest.a24bf8a04757cc57afd82bb4503e3852.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47acfa16a51ef5c1889c",
+    "url": "/css/app.f199fea1.css"
+  },
+  {
+    "revision": "ec0e0c6d2c12b3a2ccd6",
+    "url": "/css/chunk-vendors.84bb20f7.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "77b53b349a2518a665446e85a2c7f7fb",
+    "url": "/img/shopping.77b53b34.png"
+  },
+  {
+    "revision": "b576038c1ad096beae9950722034a68e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47acfa16a51ef5c1889c",
+    "url": "/js/app.654037fb.js"
+  },
+  {
+    "revision": "ec0e0c6d2c12b3a2ccd6",
+    "url": "/js/chunk-vendors.6e0e5707.js"
+  },
+  {
+    "revision": "e22af7651c603734aafb1a6fb750c841",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
